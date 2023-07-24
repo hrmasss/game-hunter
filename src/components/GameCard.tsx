c28c4747651846634000ecd62311f840f0832c3a
hrmasss <hojayfarahman@gmail.com>
2023-07-24 11:04:52 +0600

    Added critic score badge

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CriticScoreBadge from "./CriticScoreBadge";
 
 interface Props {
 	game: Game;
@@ -8,13 +9,18 @@ interface Props {
 
 export default function GameCard({ game }: Props) {
 	return (
-		<Card borderRadius={10} overflow={"hidden"}>
+		<Card borderRadius={10} overflow="hidden">
 			<Image src={game.background_image} />
 			<CardBody>
 				<Heading fontSize={"2xl"}>{game.name}</Heading>
-				<PlatformIconList
-					platforms={game.parent_platforms.map((platform) => platform.platform)}
-				/>
+				<HStack justify="space-between">
+					<PlatformIconList
+						platforms={game.parent_platforms.map(
+							(platform) => platform.platform
+						)}
+					/>
+					<CriticScoreBadge score={game.metacritic} />
+				</HStack>
 			</CardBody>
 		</Card>
 	);
