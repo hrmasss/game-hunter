01ef93620b87e65a4ed7773f99ccf883f14894d2
hrmasss <hojayfarahman@gmail.com>
2023-07-25 00:10:38 +0600

    Added genre loading skeletons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,29 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+	HStack,
+	Image,
+	List,
+	ListItem,
+	Skeleton,
+	SkeletonText,
+	Text,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 export default function GenreList() {
-	const { genres } = useGenres();
+	const { genres, error, isLoading } = useGenres();
+	const genreSkeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	return (
 		<List>
+			{(isLoading || error) &&
+				genreSkeletons.map((index) => (
+					<ListItem key={index} paddingY="5px">
+						<HStack>
+							<Skeleton boxSize="48px" borderRadius={4} />
+							<SkeletonText width="64px" />
+						</HStack>
+					</ListItem>
+				))}
 			{genres.map((genre) => (
 				<ListItem key={genre.id} paddingY="5px">
 					<HStack>
