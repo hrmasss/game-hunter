a68d67b6531f3ce18d1dea3ee05df5d6f59a0cdd
hrmasss <hojayfarahman@gmail.com>
2023-07-26 16:40:49 +0600

    Fixed Genre List Wrapping

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
 	Button,
 	HStack,
+	Heading,
 	Image,
 	List,
 	ListItem,
@@ -18,39 +19,50 @@ interface Props {
 
 export default function GenreList({ onSelectGenre, selectedGenre }: Props) {
 	const { genres, error, isLoading } = useGenres();
-	const genreSkeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	const genreSkeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	return (
-		<List>
-			{(isLoading || error) &&
-				genreSkeletons.map((index) => (
-					<ListItem key={index} paddingY="5px">
-						<HStack>
-							<Skeleton boxSize="48px" borderRadius={4} />
-							<SkeletonText width="64px" />
-						</HStack>
+		<>
+			<Heading fontSize="2xl" marginBottom={3}>
+				Genres
+			</Heading>
+			<List>
+				{(isLoading || error) &&
+					genreSkeletons.map((index) => (
+						<ListItem key={index} paddingY="5px">
+							<HStack>
+								<Skeleton boxSize="48px" borderRadius={4} />
+								<SkeletonText width="64px" />
+							</HStack>
+						</ListItem>
+					))}
+				{genres.map((genre) => (
+					<ListItem key={genre.id} paddingY="5px">
+						<Button
+							whiteSpace="normal"
+							variant="link"
+							onClick={() => onSelectGenre(genre)}
+						>
+							<HStack>
+								<Image
+									src={getCroppedImageUrl(genre.image_background)}
+									boxSize="48px"
+									objectFit="cover"
+									borderRadius={4}
+								/>
+								<Text
+									align="left"
+									fontSize="lg"
+									fontWeight={
+										genre.id === selectedGenre?.id ? "bold" : "normal"
+									}
+								>
+									{genre.name}
+								</Text>
+							</HStack>
+						</Button>
 					</ListItem>
 				))}
-			{genres.map((genre) => (
-				<ListItem key={genre.id} paddingY="5px">
-					<Button variant="link" onClick={() => onSelectGenre(genre)}>
-						<HStack>
-							<Image
-								src={getCroppedImageUrl(genre.image_background)}
-								boxSize="48px"
-								objectFit="cover"
-								borderRadius={4}
-							/>
-							<Text
-								fontSize="lg"
-								isTruncated
-								fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-							>
-								{genre.name}
-							</Text>
-						</HStack>
-					</Button>
-				</ListItem>
-			))}
-		</List>
+			</List>
+		</>
 	);
 }
